4a0205e927b8a28413959b28d9afa4d1324c0232
Nimesh <nimeshsuryavanshi36@gmail.com>
2026-03-11 18:54:46 +0530

    this is commit for front end

--- app.py
+++ app.py
@@ -13,31 +13,44 @@ reg_model = pickle.load(open("regmodel.pkl", "rb"))
 scaler = pickle.load(open("scaling.pkl", "rb"))
 
 
-# home page
+# Home Page
 @app.route("/")
 def home():
     return render_template("home.html")
 
 
-# prediction API
+# Prediction using API (Postman)
 @app.route("/predict_api", methods=["POST"])
 def predict_api():
 
     data = request.json
     print(data)
 
-    # convert values to list
     new_data = np.array(list(data.values())).reshape(1, -1)
 
-    # scale input
     new_data = scaler.transform(new_data)
 
-    # prediction
     output = reg_model.predict(new_data)
 
     return jsonify({"output": output[0]})
 
 
-# run app
+# Prediction from Frontend Form
+@app.route("/predict", methods=["POST"])
+def predict():
+
+    data = [float(x) for x in request.form.values()]
+
+    final_input = scaler.transform(np.array(data).reshape(1, -1))
+
+    output = reg_model.predict(final_input)[0]
+
+    return render_template(
+        "home.html",
+        prediction_text="House Price Prediction is {}".format(output)
+    )
+
+
+# Run app
 if __name__ == "__main__":
     app.run(debug=True)
